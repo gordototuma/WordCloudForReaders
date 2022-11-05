88835da88890a128e46b6cf45c3d5823fc10e9a4
Luis Alejandro <luisgonzalezm331@gmail.com>
2022-11-04 20:25:52 -0500

    add topics

---
+++ data/topic.py
@@ -0,0 +1,45 @@
+import textrazor
+from googletrans import Translator
+import resources.creds as credentials
+
+class Topic():
+
+    def __init__(self, text):
+        self._text = text
+        self._topics_from_txt = set()
+        self._topic = self._topics()
+        self._topic_in_dic(self._topic)
+        self._translate_topics()
+
+
+    @property
+    def getTopics(self):
+        return self._topics_from_txt
+
+    
+    def _topics(self):
+        textrazor.api_key = credentials.api_key
+        client = textrazor.TextRazor(extractors=["entities", "topics"])
+        response = client.analyze(self._text)        
+        return response
+    
+
+    def _topic_in_dic(self, response_topics):       
+
+        for entity in response_topics.topics():    
+            if(0.5<=entity.score <=1):                
+                self._topics_from_txt.add(entity.label)                
+            else: break        
+    
+
+    def _translate_topics(self):        
+
+        topics = self._topics_from_txt
+        self._topics_from_txt = set()
+        translator = Translator()
+
+        for x in topics:            
+            translation = translator.translate(x, dest='es')
+            self._topics_from_txt.add(translation.text)
+
+                
--- main.py
+++ main.py
@@ -2,23 +2,28 @@ from data.textReader import textReader
 from data.wordProcessor import wordProcessor
 from data.wordCloud import wordCloud
 from data.dictionary import Dictionary
+from data.topic import Topic
 
-txt = "Ingrese acá su lectura"
+txt = ''
 
 my_txt = textReader(txt)
 data_txt = wordProcessor(my_txt.list_words)
+topics = Topic(txt)
 
-print(my_txt.list_words)
-print(data_txt.dic_words_frequency)
+# print(my_txt.list_words)
+# print(data_txt.dic_words_frequency)
 
 #genera nube de palabras
 
-wordClouds = wordCloud(data_txt.dic_words_frequency)
-wordClouds.generate_wordcloud_image()
+# wordClouds = wordCloud(data_txt.dic_words_frequency)
+# wordClouds.generate_wordcloud_image()
 
 # definiciones de las palabras
 
-for x in data_txt.dic_words_frequency.keys():
-    glossary = Dictionary(x)
-    print(x+':',glossary.definitions)
-    print('\n')
+# for x in data_txt.dic_words_frequency.keys():
+#     glossary = Dictionary(x)
+#     print(x+':',glossary.definitions)
+#     print('\n')
+
+#Temas del texto
+#print(topics.getTopics)
