dee1238962e3c56b148b65c6234cefd3a6ab5715
Luis Alejandro <luisgonzalezm331@gmail.com>
2022-11-14 12:32:14 -0500

    add gui, txt and btn

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from data.topic import Topic
 from data.wordRelationship import wordRelationship
 from data.relationshipGraph import RelationshipGraph
 from data.diagrams import Diagrams
+from userInterface.mainWindow import MainWindow
 
-txt = 'La heroína es un polvo blanco o marrón o una sustancia pegajosa negra. Es una droga opioide proveniente de la morfina, una sustancia natural en el capullo de la amapola o adormidera asiática. Se puede mezclar con agua y se inyecta con una aguja. La heroína también puede ser fumada o inhalada por la nariz. Todas estas formas de consumir heroína la envían al cerebro muy rápido, lo que la hace muy adictiva. Los principales problemas de salud de la heroína incluyen abortos espontáneos, infecciones del corazón y muerte por sobredosis. Las personas que se inyectan la droga también corren el riesgo de contraer enfermedades infecciosas, incluyendo el VIH/SIDA y la hepatitis. El uso regular de la heroína puede conducir a su tolerancia. Esto significa que los usuarios necesitan más cantidad de la droga para tener el mismo efecto. Las dosis altas generan con el tiempo dependencia a la heroína. Si los usuarios dependientes dejan la droga, tienen síntomas de abstinencia. Estos incluyen agitación, dolor muscular y óseo, diarrea, vómitos y escalofríos con "piel de gallina".'
 
-my_txt = textReader(txt)
-data_txt = wordProcessor(my_txt.list_words)
-topics = Topic(txt)
+# txt = 'Ingrese aca su texto'
+
+# my_txt = textReader(txt)
+# data_txt = wordProcessor(my_txt.list_words)
+# topics = Topic(txt)
 
 # print(my_txt.list_words)
-print(data_txt.dic_words_frequency)
+# print(data_txt.dic_words_frequency)
 
 #genera nube de palabras
 
@@ -29,7 +31,7 @@ print(data_txt.dic_words_frequency)
 #     print('\n')
 
 #Temas del texto
-print(topics.getTopics)
+# print(topics.getTopics)
 
 #Relacion entre tema y palabras de la nube
 #relation = wordRelationship(data_txt.dic_words_frequency, topics.getTopics)
@@ -40,9 +42,8 @@ print(topics.getTopics)
 
 #Diagrama de baras - Torta
 
-diagram_pie = Diagrams(data_txt.dic_words_frequency,topics.getTopics)
-diagram_pie.graph_pie()
+# diagram_pie = Diagrams(data_txt.dic_words_frequency,topics.getTopics)
+# diagram_pie.graph_pie()
 
 
-
-
+init_window = MainWindow()
---
+++ userInterface/mainWindow.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+
+class MainWindow(tk.Tk):
+    
+    def __init__(self):
+
+        super().__init__()
+        self.title('Word Cloud For Readers')
+        self._display_frame = None
+        self._txt1 = None
+        self._btn1 = None
+        self._create_main_frame()
+        self._create_txt_input()
+        self._scrollBar_txt()
+        self._create_btn()
+        self.mainloop()
+
+    
+    def _create_main_frame(self):
+
+        self._display_frame = tk.Frame(master=self)
+        self._display_frame.pack(fill=tk.X)
+
+
+    def _create_txt_input(self):           
+
+        self._txt1 = tk.Text(
+            master=self._display_frame,
+            )
+        
+        self._txt1.grid(
+            row=0,
+            column=0,
+            padx=10,
+            pady=10
+        )
+
+    
+    def _scrollBar_txt(self):
+        
+        scroll_vert = tk.Scrollbar(
+            master=self._display_frame,
+            command=self._txt1.yview
+            )
+
+        scroll_vert.grid(
+            row=0,
+            column=1,
+            sticky='nsew'
+            )
+        
+        self._txt1.config(yscrollcommand=scroll_vert.set)
+    
+    
+    def _create_btn(self):
+
+        self._btn1 = tk.Button(
+            master=self,
+            text='Analizar Texto'
+            )
+        
+        self._btn1.pack()
+        
